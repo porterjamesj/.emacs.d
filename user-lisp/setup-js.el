8defe1d788aa069b72dbfa15138e8c867e15fa4e
James Porter <porterjamesj@gmail.com>
2013-07-23 00:43:20 -0400

    Make js2-mode checker ignore common global names

--- user-lisp/setup-js.el
+++ user-lisp/setup-js.el
@@ -4,4 +4,7 @@
 ;; set javascript mode tab width to 2
 (setq js-indent-level 2)
 
+;; make js2 linter ignore common globals
+(setq js2-global-externs '("module" "require" "$" "_"))
+
 (provide 'setup-js)
