45fadaa220f03c1611cb4cedb7d9f6588b60c8c0
James Porter <porterjamesj@gmail.com>
2013-07-23 01:15:22 -0400

    Give a default virtualenv so that jedi can at least load if there isn't one specified in .dir-locals

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -5,4 +5,7 @@
 ;; use flake8 for checking
 (setq python-check-command "flake8")
 
+;; set the default virtualenv so that jedi can at least load
+(setq python-shell-virtualenv-path "/Users/james/.virtualenvs/base")
+
 (provide 'setup-python)
