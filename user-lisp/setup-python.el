01d0d5188cbf979ce1406eaa16517196730bd97e
James Porter <porterjamesj@gmail.com>
2013-07-27 12:50:15 -0400

    Turn off jedi autocomplete on dot.

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -1,6 +1,6 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t)
+(setq jedi:complete-on-dot nil)
 
 ;; use flake8 for checking
 (setq python-check-command "flake8")
