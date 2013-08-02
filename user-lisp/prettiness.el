caf77c39ff762619e2de00f3602dadc500f010f2
James Porter <porterjamesj@gmail.com>
2013-08-02 14:57:26 -0400

    Add Consolas as fallback font for Greek.

--- user-lisp/prettiness.el
+++ user-lisp/prettiness.el
@@ -4,6 +4,17 @@
 ;; Set font
 (set-face-attribute 'default nil :font "Source Code Pro-13")
 
+;; Use Consolas as fallback for Greek
+(set-fontset-font "fontset-default"
+                  (cons (decode-char 'ucs #x1f00)
+                        (decode-char 'ucs #x1fff))
+                  "Consolas-15")
+
+(set-fontset-font "fontset-default"
+                  (cons (decode-char 'ucs #x0370)
+                        (decode-char 'ucs #x03ff))
+                  "Consolas-15")
+
 ;; Custom colors
 (load-theme 'graham t)
 
