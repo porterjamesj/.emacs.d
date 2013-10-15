cb639740ec2066984e71d5cf3bdedbbed6d5cf42
James Porter <porterjamesj@gmail.com>
2013-10-15 15:53:09 -0500

    Make dired reuse existing buffer by default.

--- user-lisp/setup-dired.el
+++ user-lisp/setup-dired.el
@@ -4,4 +4,8 @@
 (dired-details-install)
 (put 'dired-find-alternate-file 'disabled nil)
 
+;; reuse the buffer
+(require 'dired)
+(define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
+
 (provide 'setup-dired)
