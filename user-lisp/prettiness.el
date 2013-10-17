64a239849310f869e549cdf56494d0d962674779
James Porter <porterjamesj@gmail.com>
2013-10-17 03:10:14 -0500

    make it work at cli

--- user-lisp/prettiness.el
+++ user-lisp/prettiness.el
@@ -2,18 +2,19 @@
 ;; Visual settings and appearance tweaks
 
 ;; Set font
-(set-face-attribute 'default nil :font "Source Code Pro-13")
+(if window-system
+    (set-face-attribute 'default nil :font "Source Code Pro-13")
 
-;; Use Consolas as fallback for Greek
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x1f00)
-                        (decode-char 'ucs #x1fff))
-                  "Consolas-15")
+  ;; Use Consolas as fallback for Greek
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x1f00)
+                          (decode-char 'ucs #x1fff))
+                    "Consolas-15")
 
-(set-fontset-font "fontset-default"
-                  (cons (decode-char 'ucs #x0370)
-                        (decode-char 'ucs #x03ff))
-                  "Consolas-15")
+  (set-fontset-font "fontset-default"
+                    (cons (decode-char 'ucs #x0370)
+                          (decode-char 'ucs #x03ff))
+                    "Consolas-15"))
 
 ;; Custom colors
 (load-theme 'graham t)
