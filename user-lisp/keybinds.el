736632ce4481356895f19351462a8d63d462a5c1
James Porter <porterjamesj@gmail.com>
2013-10-27 23:45:06 -0500

    arrow keys . . . never again :(

--- user-lisp/keybinds.el
+++ user-lisp/keybinds.el
@@ -1,6 +1,7 @@
 ;; Key bindings
 
 (setq ns-function-modifier 'hyper) ;; fn key is hyper
+(setq ns-esc-modifier 'hyper)
 
 ;;  buffers and files
 (global-set-key (kbd "C-x C-k") 'kill-buffer-and-window)
@@ -19,7 +20,6 @@
 (global-set-key (kbd "s-b") 'fast-forward)
 (global-set-key (kbd "M-b") 'backward-word)
 
-
 ;; make scrolling more smooth
 (global-set-key (kbd "<wheel-down>") 'scroll-up-line)
 (global-set-key (kbd "<wheel-up>") 'scroll-down-line)
@@ -27,10 +27,60 @@
 (global-set-key (kbd "s-<wheel-up>") 'scroll-down)
 
 ;; buffer navigation
-(global-set-key (kbd "C-x <right>") 'next-non-emacs-buffer)
-(global-set-key (kbd "C-x <left>") 'previous-non-emacs-buffer)
-(global-set-key (kbd "C-x C-<right>") 'next-non-emacs-buffer)
-(global-set-key (kbd "C-x C-<left>") 'previous-non-emacs-buffer)
+(global-set-key (kbd "H-l") 'next-non-emacs-buffer)
+(global-set-key (kbd "H-j") 'previous-non-emacs-buffer)
+
+;; actual navigation
+(global-set-key (kbd "s-K") 'forward-paragraph)
+(global-set-key (kbd "s-I") 'backward-paragraph)
+(global-set-key (kbd "s-L") 'forward-word)
+(global-set-key (kbd "s-J") 'backward-word)
+
+(global-set-key (kbd "s-i") 'previous-line)
+(global-set-key (kbd "s-k") 'next-line)
+(global-set-key (kbd "s-l") 'forward-char)
+(global-set-key (kbd "s-j") 'backward-char)
+
+;; mark-active minor mode
+;; thanks to textpatten on stackoverflow: http://stackoverflow.com/questions/5651327/how-to-define-keybinding-inside-a-marked-region
+;; thank goodness we can hook things onto changes to the mark
+
+(defvar active-region-mode-map
+  (let ((map (make-sparse-keymap)))
+    map)
+  )
+
+(define-minor-mode active-region-mode
+  "Active Region minor mode."
+  :init-value nil
+  :lighter " Region"
+  :keymap active-region-mode-map
+  :group 'active-region
+  )
+
+(defun active-region-on ()
+  (active-region-mode 1))
+(defun active-region-off ()
+  (active-region-mode -1))
+(add-hook 'activate-mark-hook 'active-region-on)
+(add-hook 'deactivate-mark-hook 'active-region-off)
+
+;; ^ this allows for easier navigation when the mark is active
+(define-key active-region-mode-map (kbd "i") 'previous-line)
+(define-key active-region-mode-map (kbd "k") 'next-line)
+(define-key active-region-mode-map (kbd "l") 'forward-char)
+(define-key active-region-mode-map (kbd "j") 'backward-char)
+
+(define-key active-region-mode-map (kbd "i") 'backward-paragraph)
+(define-key active-region-mode-map (kbd "k") 'forward-paragraph)
+(define-key active-region-mode-map (kbd "l") 'forward-word)
+(define-key active-region-mode-map (kbd "j") 'backward-word)
+
+;; arrow keys are evil, don't use them
+(global-set-key (kbd "<up>") nil)
+(global-set-key (kbd "<down>") nil)
+(global-set-key (kbd "<left>") nil)
+(global-set-key (kbd "<right>") nil)
 
 (global-set-key (kbd "C-c o") 'switch-to-minibuffer)
 
