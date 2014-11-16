d73904dd3ba686fc8803366e39942b856b9d4352
James Porter <porterjamesj@gmail.com>
2014-11-15 21:51:59 -0600

    markdown fixes

--- user-lisp/setup-markdown.el
+++ user-lisp/setup-markdown.el
@@ -8,4 +8,8 @@
 (define-key markdown-mode-map (kbd "M-n") 'forward-paragraph)
 (define-key markdown-mode-map (kbd "M-p") 'backward-paragraph)
 
+;; fix M-b and M-f why does this mode override so many useful keys :/
+(define-key markdown-mode-map (kbd "M-f") 'forward-word)
+(define-key markdown-mode-map (kbd "M-b") 'backward-word)
+
 (provide 'setup-markdown)
