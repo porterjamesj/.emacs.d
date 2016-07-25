71ef63fd29e3d6b38d15920ac31afd08c55e4339
James Porter <porterjamesj@gmail.com>
2016-07-24 20:42:55 -0400

    use ipython as default shell

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -4,18 +4,15 @@
 (add-hook 'python-mode-hook
           (lambda ()
             (setq comint-process-echoes t)
-            (jedi:setup)
+            ;; (jedi:setup)
             (unless (file-remote-p default-directory)
               (flycheck-mode))))
 
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot nil)
+;; (setq jedi:setup-keys t)
+;; (setq jedi:complete-on-dot nil)
 
 ;; use flake8 for checking
 (setq python-check-command "flake8")
-
-;; set the default virtualenv so that jedi can at least load
-(setq python-shell-virtualenv-path "/Users/james/.virtualenvs/base")
 
 ;; setup virtualenvwrapper
 (setq venv-location "~/.virtualenvs/")
@@ -23,5 +20,15 @@
 (venv-initialize-interactive-shells)
 
 (add-to-list 'auto-mode-alist '("\\.wsgi\\'" . python-mode))
+
+(setq
+  python-shell-interpreter "ipython"
+  python-shell-interpreter-args "-i")
+
+(add-hook 'comint-mode-hook
+  (function (lambda ()
+              (local-set-key (kbd "<up>") 'comint-previous-input)
+              (local-set-key (kbd "<down>") 'comint-next-input)
+              (local-set-key (kbd "C-r") 'comint-history-isearch-backward))))
 
 (provide 'setup-python)
