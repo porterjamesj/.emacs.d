379f72bf44b39fa35c92df22f9fab788e2fdc856
James Porter <james@jamesporter.me>
2016-09-12 15:07:28 -0400

    fix m-dash
    which was broken by undo-tree mode

--- user-lisp/setup-undo-tree.el
+++ user-lisp/setup-undo-tree.el
@@ -1,4 +1,8 @@
 (global-undo-tree-mode)
 
+;; gross hack because undo-tree overrides this binding there's
+;; probably a much better way to do this without having to put this
+;; line in this file but oh well
+(global-set-key (kbd "M-_") 'insert-m-dash)
 
 (provide 'setup-undo-tree)
