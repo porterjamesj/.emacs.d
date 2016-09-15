71f38f703af1552324a0da938ddf892f43daff38
James Porter <james@jamesporter.me>
2016-09-15 02:01:29 -0400

    try ido vertical
    i tried ivy first but i couldn't get it to do all the useful things ido
    does (e.g. remembering history when opening files and autofilling a
    reasonable path). maybe i will try helm next

--- user-lisp/setup-ido.el
+++ user-lisp/setup-ido.el
@@ -1,15 +1,26 @@
-
-;; ido mode
 (require 'ido)
+(require 'flx-ido)
+(require 'ido-grid-mode)
+(require 'ido-ubiquitous)
+
 (ido-mode t)
 
-;; ido flx matching
-(require 'flx-ido)
 (flx-ido-mode 1)
 
-;; ido-ubiquitous
-;; better than ido-everywhere
-(require 'ido-ubiquitous)
 (ido-ubiquitous t)
+
+(ido-grid-mode 1)
+
+(setq ido-grid-mode-max-columns 1)
+(setq ido-grid-mode-max-rows 8)
+
+(setq ido-grid-mode-keys nil)
+
+(add-hook 'ido-setup-hook
+          (lambda ()
+            (define-key ido-completion-map (kbd "C-p") 'ido-grid-mode-up)
+            (define-key ido-completion-map (kbd "C-n") 'ido-grid-mode-down)
+            (define-key ido-completion-map (kbd "<up>") 'ido-grid-mode-up)
+            (define-key ido-completion-map (kbd "<down>") 'ido-grid-mode-down)))
 
 (provide 'setup-ido)
