7f3b8955657968fafac739a56e51b80d791246bb
James Porter <james@jamesporter.me>
2016-09-18 12:03:05 -0400

    get rid of old mc keybinds

--- user-lisp/setup-mc.el
+++ user-lisp/setup-mc.el
@@ -1,11 +1,6 @@
 (require 'multiple-cursors)
 
-;; (add-to-list 'mc/unsupported-minor-modes 'smartparens-mode)
-
 ;; multiple cursors
-(global-set-key (kbd "C-'") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-\"") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-;") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-M-<mouse-1>") 'mc/add-cursor-on-click)
 
 (provide 'setup-mc)
