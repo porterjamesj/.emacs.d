e195e874fc358565eb0a1302ce04fb94ea738f19
James Porter <james@jamesporter.me>
2017-11-12 11:16:24 -0500

    js tweaks

--- user-lisp/setup-js.el
+++ user-lisp/setup-js.el
@@ -1,9 +1,9 @@
 ;; js2-mode
 
 ;; work with react jsx files
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-jsx-mode))
-(add-to-list 'auto-mode-alist '("\\.es6\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+(add-to-list 'auto-mode-alist '("\\.es6\\'" . rjsx-mode))
 
 ;; set javascript mode tab width to 2
 (setq js-indent-level 2)
@@ -20,12 +20,14 @@
 ;; set indetation to 2 in jsx
 (defun set-jsx-indentation ()
   (setq-local sgml-basic-offset js2-basic-offset))
-(add-hook 'js2-jsx-mode-hook #'set-jsx-indentation)
+(add-hook 'rjsx-mode-hook #'set-jsx-indentation)
 
 ;; make js2 linter ignore common globals
 (setq js2-global-externs '("module" "require" "$" "_"
                            "window" "$SCRIPT_ROOT" "d3"
                            "describe" "it" "exports"
                            "chrome" "fetch"))
+
+(setq js2-strict-trailing-comma-warning nil)
 
 (provide 'setup-js)
